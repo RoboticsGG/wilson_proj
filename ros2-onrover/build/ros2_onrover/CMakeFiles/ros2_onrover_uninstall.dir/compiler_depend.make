2eddc408be146b096441bbbf61ce2e8e5b449145
wilsonSpa <viewjirapat@gmail.com>
2024-08-26 16:46:32 +0700

    config ros2-onrover Makefile

---
+++ ros2-onrover/build/ros2_onrover/CMakeFiles/ros2_onrover_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for ros2_onrover_uninstall.
+# This may be replaced when dependencies are built.
---
+++ ros2-onrover/src/text_receiver/build/text_receiver/CMakeFiles/ethernet_sub_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ethernet_sub_node.dir/src/ethernet_sub_node.cpp.o"
+  "CMakeFiles/ethernet_sub_node.dir/src/ethernet_sub_node.cpp.o.d"
+  "ethernet_sub_node"
+  "ethernet_sub_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ethernet_sub_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

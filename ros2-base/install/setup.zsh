402bad3fd94d7c73427adf4ef9273d09e01670b8
wilsonSpa <viewjirapat@gmail.com>
2024-08-26 19:08:45 +0700

    service commnicate - 1 - client can not receive response

--- ros2-base/install/setup.zsh
+++ ros2-base/install/setup.zsh
@@ -24,6 +24,9 @@ _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
 COLCON_CURRENT_PREFIX="/home/yupi/rover_proj/mros2-host-examples-main/install"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
+# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
+COLCON_CURRENT_PREFIX="/home/yupi/rover_proj/ros2-onrover/install"
+_colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script

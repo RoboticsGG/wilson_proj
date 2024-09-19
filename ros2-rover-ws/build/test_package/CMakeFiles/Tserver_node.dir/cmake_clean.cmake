b0cf9d4d09fa07112dd8cd199d5e98c7db228955
ViewRover <viewjirapat@gmail.com>
2024-09-19 21:28:17 +0700

    Update From Rover

---
+++ ros2-rover-ws/build/test_package/CMakeFiles/Tserver_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tserver_node.dir/src/Tserver_node.cpp.o"
+  "CMakeFiles/Tserver_node.dir/src/Tserver_node.cpp.o.d"
+  "Tserver_node"
+  "Tserver_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tserver_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros2-rover-ws/build/test_package/CMakeFiles/test_package__rosidl_generator_c.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_package__rosidl_generator_c.dir/rosidl_generator_c/test_package/action/detail/fibonacci__description.c.o"
+  "CMakeFiles/test_package__rosidl_generator_c.dir/rosidl_generator_c/test_package/action/detail/fibonacci__description.c.o.d"
+  "CMakeFiles/test_package__rosidl_generator_c.dir/rosidl_generator_c/test_package/action/detail/fibonacci__functions.c.o"
+  "CMakeFiles/test_package__rosidl_generator_c.dir/rosidl_generator_c/test_package/action/detail/fibonacci__functions.c.o.d"
+  "CMakeFiles/test_package__rosidl_generator_c.dir/rosidl_generator_c/test_package/action/detail/fibonacci__type_support.c.o"
+  "CMakeFiles/test_package__rosidl_generator_c.dir/rosidl_generator_c/test_package/action/detail/fibonacci__type_support.c.o.d"
+  "libtest_package__rosidl_generator_c.pdb"
+  "libtest_package__rosidl_generator_c.so"
+  "rosidl_generator_c/test_package/action/detail/fibonacci__description.c"
+  "rosidl_generator_c/test_package/action/detail/fibonacci__functions.c"
+  "rosidl_generator_c/test_package/action/detail/fibonacci__functions.h"
+  "rosidl_generator_c/test_package/action/detail/fibonacci__struct.h"
+  "rosidl_generator_c/test_package/action/detail/fibonacci__type_support.c"
+  "rosidl_generator_c/test_package/action/detail/fibonacci__type_support.h"
+  "rosidl_generator_c/test_package/action/fibonacci.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_package__rosidl_generator_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

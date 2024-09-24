ddd301c2ea601e1957056e2b6606795ab0623e6f
ViewAJirapat <viewjirapat@gmail.com>
2024-09-24 16:04:13 +0700

    ros2_base

---
+++ ros2-base-ws/build/base_command_package/rosidl_cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(base_command_package_IDL_FILES "action/Carcontrol.idl")
+set(base_command_package_INTERFACE_FILES "action/Carcontrol.action")

7fe343f95d3291f2ef8780a08d7e2dfc0d388f5b
ViewRover <viewjirapat@gmail.com>
2024-09-24 15:59:16 +0700

    push from rover

---
+++ ros2-rover-ws/build/rover_control_package/CMakeFiles/carcontrol_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/carcontrol_server.dir/rclcpp_components/node_main_carcontrol_server.cpp.o"
+  "CMakeFiles/carcontrol_server.dir/rclcpp_components/node_main_carcontrol_server.cpp.o.d"
+  "carcontrol_server"
+  "carcontrol_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/carcontrol_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

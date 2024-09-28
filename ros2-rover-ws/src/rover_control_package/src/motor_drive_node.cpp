754b3dd54e631a5cf3cb2c6e43e4364cba0ffd6d
View A Jirapat <viewjirapat@gmail.com>
2024-09-28 15:42:59 +0700

    Add Debug

--- ros2-rover-ws/src/rover_control_package/src/motor_drive_node.cpp
+++ ros2-rover-ws/src/rover_control_package/src/motor_drive_node.cpp
@@ -39,8 +39,8 @@ namespace carcontrol_cpp
       rclcpp_action::GoalResponse handle_goal(
         const rclcpp_action::GoalUUID & uuid,
         std::shared_ptr<const Carcontrol::Goal> goal)
-        RCLCPP_INFO(this->get_logger(), goal);
       {
+        RCLCPP_INFO(this->get_logger(), goal);
         RCLCPP_INFO(this->get_logger(), "Received goal request with direction %s", goal->direction.c_str());
         RCLCPP_INFO(this->get_logger(), "Received goal request with timestop %d", goal->timestop);
         (void)uuid;

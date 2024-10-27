f1490cff313b9171b9f6be6f6636a7e3e8c71059
View A Jirapat <viewjirapat@gmail.com>
2024-10-27 14:45:04 +0700

    CMake Test_commu

--- ros2-base-ws/src/test_package/src/Test_eth_commu_subscribe.cpp
+++ ros2-base-ws/src/test_package/src/Test_eth_commu_subscribe.cpp
@@ -1,36 +1,24 @@
 #include <rclcpp/rclcpp.hpp>
-#include <std_srvs/srv/trigger.hpp>
-#include <chrono>
+#include <std_msgs/msg/string.hpp>
 
-using namespace std::chrono_literals;
-
-class EthernetClient : public rclcpp::Node {
+class EthernetSubscriber : public rclcpp::Node {
 public:
-    EthernetClient() : Node("ethernet_client") {
-        client_ = this->create_client<std_srvs::srv::Trigger>("send_text");
-        timer_ = this->create_wall_timer(
-            2000ms, std::bind(&EthernetClient::send_request, this));
+    EthernetSubscriber() : Node("ethernet_subscriber") {
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "to_ros2", 10, std::bind(&EthernetSubscriber::topic_callback, this, std::placeholders::_1));
     }
 
 private:
-    void send_request() {
-        auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
-        auto result_future = client_->async_send_request(request);
-
-        if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) ==
-            rclcpp::FutureReturnCode::SUCCESS) {
-            RCLCPP_INFO(this->get_logger(), "Result: %s", result_future.get()->message.c_str());
-        } else {
-            RCLCPP_ERROR(this->get_logger(), "Failed to call service send_text");
-        }
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const {
+        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
     }
-    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr client_;
-    rclcpp::TimerBase::SharedPtr timer_;
+
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<EthernetClient>());
+    rclcpp::spin(std::make_shared<EthernetSubscriber>());
     rclcpp::shutdown();
     return 0;
 }

9e6cc0e4c8bf86607b4473abc69cd3a4c3fc400f
View A Jirapat <viewjirapat@gmail.com>
2024-11-04 22:25:22 +0700

    action build

--- ros2-rover-ws/src/package_rover_control/src/node_motor_control.cpp
+++ ros2-rover-ws/src/package_rover_control/src/node_motor_control.cpp
@@ -1,39 +1,36 @@
-#include <functional>
-#include <memory>
-#include <thread>
+// #include <functional>
+// #include <memory>
+// #include <thread>
 
-//#include "package_rover_control/action/Carcontrol.action"
-#include "rclcpp/rclcpp.hpp"
-#include "rclcpp_action/rclcpp_action.hpp"
-#include "rclcpp_components/register_node_macro.hpp"
+// #include "rclcpp/rclcpp.hpp"
+// #include "rclcpp_action/rclcpp_action.hpp"
+// #include "rclcpp_components/register_node_macro.hpp"
+// #include "package_rover_control/action/Carcontrol.hpp"
 
-namespace motorcontrol_cpp
-{
-  class MotorControlActionServer : public rclcpp::Node
-  {
-    public:
-      using motorcontrol = package_rover_control::action::Carcontrol;
-      using GoalHandleCarcontrol = rclcpp_action::ServerGoalHandle<motorcontrol>;
+// namespace motorcontrol_cpp
+// {
+//   class MotorControlActionServer : public rclcpp::Node
+//   {
+//     public:
+//       explicit MotorControlActionServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions()):
+//         Node("node_motor_control", options)
+//         {
+//           using namespace std::placeholders;
 
-      explicit MotorControlActionServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions()):
-        Node("node_motor_control", options)
-        {
-          using namespace std::placeholders;
+//           this->action_server_ = rclcpp_action::create_server<motorcontrol>(
+//             "motorcontrol",
+//             std::bind(&MotorControlActionServer::handle_goal, this, _1, _2),
+//             std::bind(&MotorControlActionServer::handle_cancel, this, _1),
+//             std::bind(&MotorControlActionServer::handle_accepted, this, _1)
+//           );
+//           RCLCPP_INFO(this->het_logger(), "Motor Control Action Server is Ready.");
 
-          this->action_server_ = rclcpp_action::create_server<motorcontrol>(
-            "motorcontrol",
-            std::bind(&MotorControlActionServer::handle_goal, this, _1, _2),
-            std::bind(&MotorControlActionServer::handle_cancel, this, _1),
-            std::bind(&MotorControlActionServer::handle_accepted, this, _1)
-          );
-          RCLCPP_INFO(this->het_logger(), "Motor Control Action Server is Ready.");
+//         }
+//   }
 
-        }
-  }
+// }
 
-}
-
-RCLCPP_COMPONENTS_REGISTER_NODE(motorcontrol_cpp::CarControlActionServer)
+// RCLCPP_COMPONENTS_REGISTER_NODE(motorcontrol_cpp::CarControlActionServer)
 
 // #include <cstdio>
 

95d65c46f7b2beaeae99a248a4e5532ff40755ce
View A Jirapat <viewjirapat@gmail.com>
2024-11-04 21:20:45 +0700

    motor_node and CMake Config Test 1

---
+++ ros2-rover-ws/src/package_rover_control/src/node_motor_control.cpp
@@ -0,0 +1,47 @@
+#include <functional>
+#include <memory>
+#include <thread>
+
+#include "package_rover_control/action/carcontrol.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+
+namespace motorcontrol_cpp
+{
+  class MotorControlActionServer : public rclcpp::Node
+  {
+    public:
+      using motorcontrol = package_rover_control::action::carcontrol;
+      using GoalHandleCarcontrol = rclcpp_action::ServerGoalHandle<motorcontrol>;
+
+      explicit MotorControlActionServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions()):
+        Node("node_motor_control", options)
+        {
+          using namespace std::placeholders;
+
+          this->action_server_ = rclcpp_action::create_server<motorcontrol>(
+            "motorcontrol",
+            std::bind(&MotorControlActionServer::handle_goal, this, _1, _2),
+            std::bind(&MotorControlActionServer::handle_cancel, this, _1),
+            std::bind(&MotorControlActionServer::handle_accepted, this, _1)
+          );
+          RCLCPP_INFO(this->het_logger(), "Motor Control Action Server is Ready.");
+
+        }
+  }
+
+}
+
+RCLCPP_COMPONENTS_REGISTER_NODE(motorcontrol_cpp::CarControlActionServer)
+
+// #include <cstdio>
+
+// int main(int argc, char ** argv)
+// {
+//   (void) argc;
+//   (void) argv;
+
+//   printf("hello world package_rover_control package\n");
+//   return 0;
+// }

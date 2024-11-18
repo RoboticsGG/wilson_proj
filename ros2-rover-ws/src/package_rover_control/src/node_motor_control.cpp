6a75adecaf32d851ac8a0e55ffb4029ea4ba29c1
View A Jirapat <viewjirapat@gmail.com>
2024-11-18 14:14:04 +0700

    Node_Motor_Control - RPI4 Side

--- ros2-rover-ws/src/package_rover_control/src/node_motor_control.cpp
+++ ros2-rover-ws/src/package_rover_control/src/node_motor_control.cpp
@@ -21,15 +21,82 @@ namespace motorcontrol_cpp
         {
           using namespace std::placeholders;
 
-          // this->action_server_ = rclcpp_action::create_server<Carcontrol>(
-          //   "motorcontrol",
-          //   std::bind(&MotorControlActionServer::handle_goal, this, _1, _2),
-          //   std::bind(&MotorControlActionServer::handle_cancel, this, _1),
-          //   std::bind(&MotorControlActionServer::handle_accepted, this, _1)
-          // );
+          this->action_server_ = rclcpp_action::create_server<Carcontrol>(
+            "motorcontrol",
+            std::bind(&MotorControlActionServer::handle_goal, this, _1, _2),
+            std::bind(&MotorControlActionServer::handle_cancel, this, _1),
+            std::bind(&MotorControlActionServer::handle_accepted, this, _1)
+          );
           RCLCPP_INFO(this->get_logger(), "Motor Control Action Server is Ready.");
 
         }
+
+    private:
+      rclcpp_action::Server<Carcontrol>::SharedPtr action_server_;
+      rclcpp_action::GoalResponse handle_goal(
+        const rclcpp_action::GoalUUID & uuid,
+        std::shared_ptr<const Carcontrol::Goal> goal)
+      {
+        RCLCPP_INFO(this->get_logger(), "Received goal request: Direction = %s, Timestop = %d",
+          gola->direction.c_str(), goal->timestop);
+        (void)uuid;
+        if(gola->timestop <= 0){
+          RCLCPP_WARN(this->get_logger(), "Invalid timestop. Rejecting goal.");
+          return rclcpp_action::GoalResponse::REJECT;
+        }
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+      }
+
+      rclcpp_action::CancelResponse handle_cancel(
+        const std::shared_ptr<GoalHandleCarcontrol> goal_handle){
+        RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+        (void)goal_handle;
+        return rclcpp_action::CancelResponse::ACCEPT;
+      }
+
+      void handle_accepted(const std::shared_ptr<GoalHandleCarcontrol> goal_handle)
+      {
+        std::thread{std::bind(&MotorControlActionServer::execute, this, goal_handle)}.detect();
+      }
+
+      void execute(const std::shared_ptr<GoalHandleCarcontrol> goal_handle)
+      {
+        RCLCPP_INFO(this->get_logger(), "Executing goal");
+
+        const auto goal = goal_handle->get_goal();
+        auto feedback = std::make_shared<Carcontrol::Feedback>();
+        auto result = std::make_shared<Carcontrol::Result>();
+
+        rclcpp::Rate loop_rate(1);
+
+        for (int i = goal->timestop; i>=0 && rclcpp::ok(); --i){
+          if(goal_handle->is_canceling()){
+            RCLCPP_INFO(this->get_logger(), "Goal canceled");
+            result->result_msg = "Countdown canceled";
+            goal_handle->canceled(result);
+            return;
+          }
+
+        feedback->time_remaining.clear();
+        for (int j = i; j >= 0; --j){
+          feedback->time_remaining.push_back(j);
+        }
+
+        goal_handle->publish_feedback(feedback);
+        RCLCPP_INFO(this->get_logger(), "Publishing feedback: %d second remaining", i);
+
+        loop_rate.sleep();
+        }
+
+        if(rclcpp::ok()){
+          result->result_msg = "Countdown complete";
+          goal_handle->succeed(result);
+          RCLCPP_INFO(this->get_logger(), "Goal succeed: Countdown complete");
+        }
+
+        RCLCPP_INFO(this->get_logger(), "Waiting for next goal...");
+      }
+
   };
 
 }

890bc781a42272d8c7602ffe68dbe5ff56a69ebb
View A Jirapat <viewjirapat@gmail.com>
2024-11-29 23:11:28 +0700

    node_rover_control on ROVER and CMake

---
+++ ros2-rover-ws/src/package_rover_control/src/node_rover_control.cpp
@@ -0,0 +1,30 @@
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/int32.hpp>
+
+class Node_Rovercontrol : public rclcpp::Node {
+public:
+    Node_Rovercontrol() : Node("multi_subscriber_node"), last_b_value1_(0), last_b_value2_(0) {
+        topic_speedlimit_subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "topic_speedlimit", 10,
+            std::bind(&Node_Rovercontrol::topic_speedlimit_callback, this, std::placeholders::_1)
+        );
+        RCLCPP_INFO(this->get_logger(), "Node_Rovercontrol initialized and listening...");
+    }
+
+private:
+    void topic_speedlimit_callback(const std_msgs::msg::String::SharedPtr msg) {
+        speedlimit_message_ = msg->data;
+        RCLCPP_INFO(this->get_logger(), "Received on A_topic: '%s'", speedlimit_message_.c_str());
+    }
+
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr topic_speedlimit_subscription_;
+    std::string speedlimit_message_;
+};
+
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Node_Rovercontrol>());
+    rclcpp::shutdown();
+    return 0;
+}

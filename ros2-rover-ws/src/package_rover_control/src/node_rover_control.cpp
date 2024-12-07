3d773a01a35a2359bc91e8140c78c8cd976620c4
View A Jirapat <viewjirapat@gmail.com>
2024-12-08 02:23:39 +0700

    node_rover_control_check update

--- ros2-rover-ws/src/package_rover_control/src/node_rover_control.cpp
+++ ros2-rover-ws/src/package_rover_control/src/node_rover_control.cpp
@@ -53,23 +53,28 @@ private:
     }
 
     void timer_callback(){
-        try {
-            std::vector<int> values = parse_speedlimit_message(speedlimit_message_);
-            if (values.size() == 2){
-                Motors_Rovercontrol motor_controller;
-                int result = motor_controller.test(values[0], values[1]);
+        if (message_updated_){
+            try {
+                std::vector<int> values = parse_speedlimit_message(speedlimit_message_);
+                if (values.size() == 2){
+                    Motors_Rovercontrol motor_controller;
+                    int result = motor_controller.test(values[0], values[1]); //Example call function
 
-                auto result_msg = std_msgs::msg::Int32();
-                result_msg.data = result;
-                topic_motorcontrol_publisher_->publish(result_msg);
+                    auto result_msg = std_msgs::msg::Int32();
+                    result_msg.data = result;
+                    topic_motorcontrol_publisher_->publish(result_msg);
 
-                RCLCPP_INFO(this->get_logger(), "Published Rovercontrol result: '%d'", result);
-            } else {
-                RCLCPP_WARN(this->get_logger(), "Expected two integers, but received: '%s'", speedlimit_message_.c_str());
+                    RCLCPP_INFO(this->get_logger(), "Published Rovercontrol result: '%d'", result);
+                } else {
+                    RCLCPP_WARN(this->get_logger(), "Expected two integers, but received: '%s'", speedlimit_message_.c_str());
+                }
+            } catch(const std::exception& e) {
+                RCLCPP_INFO(this->get_logger(), "Waiting for updated messages on topic_speedlimit...");
+                // std::cerr << e.what() << '\n';
             }
-        } catch(const std::exception& e) {
+            message_updated_ = false;
+        } else {
             RCLCPP_INFO(this->get_logger(), "Waiting for updated messages on topic_speedlimit...");
-            // std::cerr << e.what() << '\n';
         }
     }
 

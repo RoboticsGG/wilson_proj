e7dad3c2c6c06ec7d9af68b38dcd943edfef6f40
View A Jirapat <viewjirapat@gmail.com>
2024-12-09 22:07:59 +0700

    mros2 node_motor

--- mros2-mbed/workspace/proj_node/Node_MotorControl/app.cpp
+++ mros2-mbed/workspace/proj_node/Node_MotorControl/app.cpp
@@ -3,11 +3,32 @@
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/u_int16.hpp"
 
-void userCallback(std_msgs::msg::UInt16 *msg){
+#include "mbed.h"
+
+DigitalIn signalPinA(PF_13);
+DigitalIn signalPinB(PF_14);
+
+PwmOut MortorRPWM(PE_11);
+PwmOut MortorLPWM(PD_15);
+
+DigitalOut MortorEN(PE_13);
+
+int encoderInA = 0;
+int encoderInB = 0;
+
+string Direction = "";
+
+void userCallback(std_msgs::msg::String *msg){
     MROS2_INFO("sub msg: '%d'", msg->data);
+    Direction = msg->data;
 }
 
 int main(){
+
+    signalPinA.mode(PullUp);
+    signalPinB.mode(PullUp); 
+    MortorEN.write(1);
+
     if (mros2_platform::network_connect()){
         MROS2_ERROR("failed to connect and setup network! aborting...");
         return -1;
@@ -25,10 +46,27 @@ int main(){
     mros2::Node node = mros2::Node::create_node("mros2_node")
     mros2::Publisher inclination = node.create_publisher<std_msgs::msg::String>("topic_roverinclination", 10);
     mros2::Publisher movefeedback = node.create_publisher<std_msgs::msg::String>("topic_movefeedback", 10);
-    mros2::Subscriber rovercontrol = node.create_subscription<std_msgs::msg::UInt16>("pub_rovercontrol", 10, userCallback)
+    mros2::Subscriber rovercontrol = node.create_subscription<std_msgs::msg::String>("pub_rovercontrol", 10, userCallback)
 
     osDelay(100);
     MROS2_INFO("ready to pub/sub message\r\n---");
+
+    if (Direction == "FW"){
+        MortorRPWM.period_us(50);
+        MortorRPWM.write(0.90f);
+        MortorLPWM.period_us(50);
+        MortorLPWM.write(0.0f);
+    } else if (Direction == "BW"){
+        MortorRPWM.period_us(50);
+        MortorRPWM.write(0.00f);
+        MortorLPWM.period_us(50);
+        MortorLPWM.write(0.90f);
+    } else {
+        MortorRPWM.period_us(50);
+        MortorRPWM.write(0.00f);
+        MortorLPWM.period_us(50);
+        MortorLPWM.write(0.00f);
+    }
 
     auto count = 0;
     while(1){

4dcaabfa1f6c3fe0aa8e4be0a64e392bd630ecac
View A Jirapat <viewjirapat@gmail.com>
2024-12-10 17:03:36 +0700

    2

--- mros2-mbed/workspace/Node_MotorControl/app.cpp
+++ mros2-mbed/workspace/Node_MotorControl/app.cpp
@@ -5,7 +5,6 @@
 #include <cstdlib>
 #include <string>
 #include <iostream>
-#include "mbed.h"
 
 using namespace std;
 
@@ -31,9 +30,7 @@ void parseDirection(const string& direction, int& speed, string& movement) {
 }
 
 void userCallback(std_msgs::msg::String *msg){
-    auto msg_received = std_msgs::msg::String();
-    msg_received = msg->data;
-    MROS2_INFO("sub msg: '%s'", msg_received.c_str());
+    MROS2_INFO("subscribed msg: '%s'", msg->data.c_str());
     Direction = msg->data;
 }
 

55f60057fb1352329c793861a8eaf117ecb996f2
View A Jirapat <viewjirapat@gmail.com>
2024-12-10 23:02:11 +0700

    mros2 update

--- mros2-mbed/workspace/proj_node/Node_MotorControl/app.cpp
+++ mros2-mbed/workspace/proj_node/Node_MotorControl/app.cpp
@@ -1,10 +1,31 @@
+/* mros2 example
+ * Copyright (c) 2022 mROS-base
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "mros2.h"
 #include "mros2-platform.h"
 #include "std_msgs/msg/string.hpp"
 
+mros2::Subscriber sub;
+mros2::Publisher pub;
+
 void userCallback(std_msgs::msg::String *msg)
 {
   MROS2_INFO("subscribed msg: '%s'", msg->data.c_str());
+  //MROS2_INFO("publishing msg: '%s'", msg->data.c_str());
+  //pub.publish(*msg);
 }
 
 int main()
@@ -27,21 +48,10 @@ int main()
   MROS2_DEBUG("mROS 2 initialization is completed");
 
   mros2::Node node = mros2::Node::create_node("mros2_node");
-  //mros2::Publisher pub = node.create_publisher<std_msgs::msg::String>("to_linux", 10);
-  mros2::Subscriber sub = node.create_subscription<std_msgs::msg::String>("topic_speedlimit", 10, userCallback);
-
+  //pub = node.create_publisher<std_msgs::msg::String>("to_linux", 10);
+  sub = node.create_subscription<std_msgs::msg::String>("pub_rovercontrol", 10, userCallback);
   osDelay(100);
-  MROS2_INFO("ready to sub message\r\n---");
-
-//   auto count = 0;
-//   while (1)
-//   {
-//     //auto msg = std_msgs::msg::String();
-//     //msg.data = "Hello from " + std::string(MROS2_PLATFORM_NAME) + " onto " + quote(TARGET_NAME) + ": " + std::to_string(count++);
-//     //MROS2_INFO("publishing msg: '%s'", msg.data.c_str());
-//     //pub.publish(msg);
-//     osDelay(1000);
-//   }
+  MROS2_INFO("ready to pub/sub message\r\n---");
 
   mros2::spin();
   return 0;

d350d41c2cd82e04cfac993f9019fd3726e72713
View A Jirapat <viewjirapat@gmail.com>
2024-12-23 14:30:30 +0700

    Test Code

--- mros2-mbed/workspace/proj_node/Node_MotorControl/app.cpp
+++ mros2-mbed/workspace/proj_node/Node_MotorControl/app.cpp
@@ -17,15 +17,11 @@
 #include "mros2.h"
 #include "mros2-platform.h"
 #include "std_msgs/msg/string.hpp"
-
-mros2::Subscriber sub;
-mros2::Publisher pub;
+#include <cstdlib>
 
 void userCallback(std_msgs::msg::String *msg)
 {
   MROS2_INFO("subscribed msg: '%s'", msg->data.c_str());
-  MROS2_INFO("publishing msg: '%s'", msg->data.c_str());
-  pub.publish(*msg);
 }
 
 int main()
@@ -43,17 +39,27 @@ int main()
   }
 
   MROS2_INFO("%s start!", MROS2_PLATFORM_NAME);
-  MROS2_INFO("Version A");
-  MROS2_INFO("app name: TestSub");
+  MROS2_INFO("app name: echoback_string");
 
   mros2::init(0, NULL);
   MROS2_DEBUG("mROS 2 initialization is completed");
 
   mros2::Node node = mros2::Node::create_node("mros2_node");
-  pub = node.create_publisher<std_msgs::msg::String>("to_linux", 8);
-  sub = node.create_subscription<std_msgs::msg::String>("pub_rovercontrol", 9, userCallback);
+  mros2::Publisher pub = node.create_publisher<std_msgs::msg::String>("to_linux", 10);
+  mros2::Subscriber sub = node.create_subscription<std_msgs::msg::String>("to_stm", 10, userCallback);
+
   osDelay(100);
   MROS2_INFO("ready to pub/sub message\r\n---");
+
+  auto count = 0;
+  while (1)
+  {
+    auto msg = std_msgs::msg::String();
+    msg.data = "Hello from " + std::string(MROS2_PLATFORM_NAME) + " onto " + quote(TARGET_NAME) + ": " + std::to_string(count++);
+    MROS2_INFO("publishing msg: '%s'", msg.data.c_str());
+    pub.publish(msg);
+    osDelay(1000);
+  }
 
   mros2::spin();
   return 0;

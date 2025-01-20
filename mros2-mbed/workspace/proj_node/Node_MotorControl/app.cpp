e520bfa680df3241c0aae76f77f6a0d601e862de
View A Jirapat <viewjirapat@gmail.com>
2025-01-20 19:33:23 +0700

    define memory pool

--- mros2-mbed/workspace/proj_node/Node_MotorControl/app.cpp
+++ mros2-mbed/workspace/proj_node/Node_MotorControl/app.cpp
@@ -22,6 +22,8 @@
 #include <string>
 #include <tuple>
 
+#define MROS2_MEMORY_POOL_SIZE 8192
+
 // void frontControl(std::string frontDirection, uint8_t diff_degree);
 // void backControl(std::string backDirection);
 // void parseCommandData(const std::string& cmData);

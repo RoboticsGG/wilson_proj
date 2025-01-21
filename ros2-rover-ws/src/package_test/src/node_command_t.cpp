eb0baed1ee9eaa7e75ee0a22e78ac7daf7a736e3
View A Jirapat <viewjirapat@gmail.com>
2025-01-21 20:13:43 +0700

    random every time

--- ros2-rover-ws/src/package_test/src/node_command_t.cpp
+++ ros2-rover-ws/src/package_test/src/node_command_t.cpp
@@ -2,11 +2,13 @@
 #include <std_msgs/msg/string.hpp>
 #include <std_msgs/msg/u_int16.hpp>
 #include <std_msgs/msg/u_int16_multi_array.hpp> 
+#include <cstdlib>
+#include <ctime>
 
 class Node_Command : public rclcpp::Node {
 public:
     Node_Command()
-    : Node("node_command"), speedlimit_(generate_random_speedlimit()) {
+    : Node("node_command") {
     // : Node("node_command"), speedlimit_(30), des_a_(0), des_b_(0), frontDirection_(90), period_PWM_(20), dutycycle_PWM_(50), backDirection_("FW") {
 
         topic_speedlimit_publisher_ = this->create_publisher<std_msgs::msg::String>("topic_speedlimit_t", 10);
@@ -26,9 +28,7 @@ public:
     }
 
 private:
-
     uint8_t generate_random_speedlimit() {
-        std::srand(static_cast<unsigned int>(std::time(nullptr))); // Seed the random number generator
         return static_cast<uint8_t>(10 + std::rand() % 90); // Generate random number between 10 and 99
     }
     // rcl_interfaces::msg::SetParametersResult on_parameter_change(
@@ -48,6 +48,7 @@ private:
 
     void publish_parameters() {
         // Pub topic_speedlimit
+        speedlimit_ = generate_random_speedlimit();
         auto speed_message = std_msgs::msg::String();
         speed_message.data = std::to_string(speedlimit_);
         RCLCPP_INFO(this->get_logger(), "Publishing to topic_speedlimit: '%s'", speed_message.data.c_str());

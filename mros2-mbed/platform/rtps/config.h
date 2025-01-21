3835f524026ecaa4cb67d5941c2a3f0ba9116a5a
View A Jirapat <viewjirapat@gmail.com>
2025-01-21 16:25:44 +0700

    config rtps config.h

--- mros2-mbed/platform/rtps/config.h
+++ mros2-mbed/platform/rtps/config.h
@@ -62,16 +62,16 @@ const uint8_t MAX_NUM_UNMATCHED_REMOTE_READERS = 15;
 const uint8_t MAX_NUM_READER_CALLBACKS = 5;
 
 
-const uint8_t HISTORY_SIZE_STATELESS = 2;
-const uint8_t HISTORY_SIZE_STATEFUL = 10;
+const uint8_t HISTORY_SIZE_STATELESS = 1; //////////////////////
+const uint8_t HISTORY_SIZE_STATEFUL = 5; ///////////////////////
 
 const uint8_t MAX_TYPENAME_LENGTH = 60;
 const uint8_t MAX_TOPICNAME_LENGTH = 40;
 
-const int HEARTBEAT_STACKSIZE = 4096;          // byte
-const int THREAD_POOL_WRITER_STACKSIZE = 4096; // byte
-const int THREAD_POOL_READER_STACKSIZE = 4096; // byte
-const uint16_t SPDP_WRITER_STACKSIZE = 4096;    // byte
+const int HEARTBEAT_STACKSIZE = 2048;          // byte //////////////////
+const int THREAD_POOL_WRITER_STACKSIZE = 2048; // byte //////////////////
+const int THREAD_POOL_READER_STACKSIZE = 2048; // byte /////////////////
+const uint16_t SPDP_WRITER_STACKSIZE = 2048;    // byte /////////////////
 
 const uint16_t SF_WRITER_HB_PERIOD_MS = 4000;
 const uint16_t SPDP_RESEND_PERIOD_MS = 1000;

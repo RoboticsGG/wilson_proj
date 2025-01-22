316b1f79c76bf2bfb46d6bcb7fabf655ee10f092
View A Jirapat <viewjirapat@gmail.com>
2025-01-22 16:27:18 +0700

    data type

--- ros2-rover-ws/src/package_image_process/package_image_process/node_image_process.py
+++ ros2-rover-ws/src/package_image_process/package_image_process/node_image_process.py
@@ -29,9 +29,9 @@ class ImageProcess(Node):
             degree_diff = self.latest_data["degree_diff"]
 
         degree_diff = round(degree_diff, 2)
-        
+
         combined_message = Float32MultiArray()
-        combined_message.data = [float(direction), float(degree_diff)]
+        combined_message.data = [float(direction), degree_diff]
 
         self.get_logger().info(f"Publishing: {combined_message.data}")
         self.publisher_.publish(combined_message)

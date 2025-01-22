172142ae1a14ea6270e2450455a99e1cde1f9ad6
View A Jirapat <viewjirapat@gmail.com>
2025-01-22 17:07:10 +0700

    debug

--- ros2-rover-ws/src/package_rover_control/src/node_rover_control_a.cpp
+++ ros2-rover-ws/src/package_rover_control/src/node_rover_control_a.cpp
@@ -1,6 +1,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
-#include <std_msgs/msg/u_int16_multi_array.hpp>
+#include <std_msgs/msg/u_int8.hpp>
+#include <std_msgs/msg/float32_multi_array.hpp>
 #include <string>
 #include <sstream>
 #include <mutex>
@@ -15,22 +16,22 @@ public:
 class Node_Rovercontrol : public rclcpp::Node {
 public:
     Node_Rovercontrol() : Node("node_rovercontrol") {
-        topic_speedlimit_subscription_ = this->create_subscription<std_msgs::msg::String>(
+        topic_spd_sub_ = this->create_subscription<std_msgs::msg::UInt8>(
             "topic_speedlimit_t", 10,
-            std::bind(&Node_Rovercontrol::topic_speedlimit_callback, this, std::placeholders::_1)
+            std::bind(&Node_Rovercontrol::topic_spd_callback, this, std::placeholders::_1)
         );
 
-        topic_destination_subscription_ = this->create_subscription<std_msgs::msg::UInt16MultiArray>(
+        topic_des_sub_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
             "topic_destination", 10,
-            std::bind(&Node_Rovercontrol::topic_destination_callback, this, std::placeholders::_1)
+            std::bind(&Node_Rovercontrol::topic_des_callback, this, std::placeholders::_1)
         );
 
-        topic_direction_subscription_ = this->create_subscription<std_msgs::msg::String>(
+        topic_direct_sub_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
             "topic_direction", 10,
-            std::bind(&Node_Rovercontrol::topic_direction_callback, this, std::placeholders::_1)
+            std::bind(&Node_Rovercontrol::topic_direct_callback, this, std::placeholders::_1)
         );
    
-        topic_rovercontrol_publisher_ = this->create_publisher<std_msgs::msg::String>("pub_rovercontrol", 10);
+        topic_rovercontrol_publisher_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("pub_rovercontrol", 10);
 
         timer_ = this->create_wall_timer(
             std::chrono::seconds(2), 
@@ -41,23 +42,23 @@ public:
     }
 
 private:
-    void topic_direction_callback(const std_msgs::msg::String::SharedPtr msg){
+    void topic_direct_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg){
         std::lock_guard<std::mutex> lock(data_lock_);
-        if (rovercontrol_message_ != msg->data){
-            rovercontrol_message_ = msg->data;
-            RCLCPP_INFO(this->get_logger(), "Received on topic_direction: '%s'", rovercontrol_message_.c_str());
+        if (ro_ctrl_msg_ != msg->data){
+            ro_ctrl_msg_ = msg->data;
+            RCLCPP_INFO(this->get_logger(), "Received on topic_direction: '%s'", ro_ctrl_msg_.c_str());
         }
     }
 
-    void topic_speedlimit_callback(const std_msgs::msg::String::SharedPtr msg) {
+    void topic_spd_callback(const std_msgs::msg::UInt8::SharedPtr msg) {
         std::lock_guard<std::mutex> lock(data_lock_);
-        if (speedlimit_message_ != msg->data){
-            speedlimit_message_ = msg->data;
-            RCLCPP_INFO(this->get_logger(), "Received on topic_speedlimit: '%s'", speedlimit_message_.c_str());
+        if (spd_msg_ != msg->data){
+            spd_msg_ = msg->data;
+            RCLCPP_INFO(this->get_logger(), "Received on topic_speedlimit: '%s'", spd_msg_.c_str());
         }
     }
 
-    void topic_destination_callback(const std_msgs::msg::UInt16MultiArray::SharedPtr msg){
+    void topic_des_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg){
         if (msg->data.size()==2) {
             std::lock_guard<std::mutex> lock(data_lock_);
             if (destination_a_ != msg->data[0] || destination_b_ != msg->data[1]){
@@ -70,32 +71,26 @@ private:
 
 
 
-    // void timer_callback(){
-    //     std::lock_guard<std::mutex> lock(data_lock_);
-    //     auto rovercon_msg = std_msgs::msg::String();
-    //     rovercon_msg.data = rovercontrol_message_ + "," + speedlimit_message_ + "," + back_direction_message_;
-    //     topic_rovercontrol_publisher_->publish(rovercon_msg);
-    //     RCLCPP_INFO(this->get_logger(), "Published to pub_rovercontrol: '%s'", rovercon_msg.data.c_str());
-    // }
 
     void timer_callback() {
-        std::stringstream ss;
-        ss << rovercontrol_message_ << "," << speedlimit_message_ << "," << back_direction_message_;
-        std_msgs::msg::String rovercon_msg;
-        rovercon_msg.data = ss.str();
-        topic_rovercontrol_publisher_->publish(rovercon_msg);
-        RCLCPP_INFO(this->get_logger(), "Published to pub_rovercontrol: '%s'", rovercon_msg.data.c_str());
+        // std::stringstream ss;
+        // ss << ro_ctrl_msg_ << "," << spd_msg_ << "," << back_direction_message_;
+        // std_msgs::msg::String rovercon_msg;
+        // rovercon_msg.data = ss.str();
+        // topic_rovercontrol_publisher_->publish(rovercon_msg);
+        // RCLCPP_INFO(this->get_logger(), "Published to pub_rovercontrol: '%s'", rovercon_msg.data.c_str());
+        RCLCPP_INFO(this->get_logger(), "------------------------------------------------");
   }
 
 
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr topic_speedlimit_subscription_;
-    rclcpp::Subscription<std_msgs::msg::UInt16MultiArray>::SharedPtr topic_destination_subscription_;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr topic_direction_subscription_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr topic_spd_sub_;
+    rclcpp::Subscription<std_msgs::msg::UInt16MultiArray>::SharedPtr topic_des_sub_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr topic_direct_sub_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr topic_rovercontrol_publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
 
-    std::string rovercontrol_message_ = "fw,0";
-    std::string speedlimit_message_ = "0";
+    std::string ro_ctrl_msg_ = "fw,0";
+    std::string spd_msg_ = "0";
     std::string back_direction_message_ = "fw";
 
     int destination_a_ = 0;

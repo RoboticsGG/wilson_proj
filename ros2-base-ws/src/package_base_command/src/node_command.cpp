6550769174f1a23ed9d68c95d4cbd918753a97cc
View A Jirapat <viewjirapat@gmail.com>
2025-01-22 12:12:37 +0700

    try add parameter

--- ros2-base-ws/src/package_base_command/src/node_command.cpp
+++ ros2-base-ws/src/package_base_command/src/node_command.cpp
@@ -9,10 +9,12 @@ class Node_Command : public rclcpp::Node {
 public:
     Node_Command()
     : Node("node_command") {
-    // : Node("node_command"), speedlimit_(30), des_a_(0), des_b_(0), frontDirection_(90), period_PWM_(20), dutycycle_PWM_(50), backDirection_("FW") {
 
         topic_speedlimit_publisher_ = this->create_publisher<std_msgs::msg::String>("topic_speedlimit_t", 10);
 
+        this->declare_parameter<uint8_t>("speedlimit", speedlimit_);
+        this->declare_parameter<int>("des_a", des_a_);
+        this->declare_parameter<int>("des_b", des_b_);
 
         timer_ = this->create_wall_timer(
             std::chrono::seconds(1),
@@ -24,13 +26,32 @@ public:
     }
 
 private:
+    rcl_interfaces::msg::SetParametersResult on_parameter_change(
+        const std::vector<rclcpp::Parameter> &parameters) {
+        rcl_interfaces::msg::SetParametersResult result;
+        result.successful = true;
+
+        for (const auto &param : parameters) {
+            if (param.get_name() == "speedlimit"){
+                speedlimit_ = static_cast<uint8_t>(param.as_int());
+            } else if (param.get_name() == "des_a" && param.get_type() == rclcpp::ParameterType::PARAMETER_INTEGER) {
+                des_a_ = param.as_int();
+            } else if (param.get_name() == "des_b" && param.get_type() == rclcpp::ParameterType::PARAMETER_INTEGER) {
+                des_b_ = param.as_int();
+            }
+        }
+
+        publish_parameters();
+        return result;
+    }
+
     uint8_t generate_random_speedlimit() {
-        return static_cast<uint8_t>(10 + std::rand() % 90); // Generate random number between 10 and 99
+        return static_cast<uint8_t>(10 + std::rand() % 90);
     }
 
     void publish_parameters() {
         // Pub topic_speedlimit
-        speedlimit_ = generate_random_speedlimit();
+        //speedlimit_ = generate_random_speedlimit();
         auto speed_message = std_msgs::msg::String();
         speed_message.data = std::to_string(speedlimit_);
         RCLCPP_INFO(this->get_logger(), "Publishing to topic_speedlimit: '%s'", speed_message.data.c_str());
@@ -38,7 +59,7 @@ private:
 
     }
 
-    uint8_t speedlimit_;
+    uint8_t speedlimit_ = 0;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr topic_speedlimit_publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr parameter_callback_handle_;

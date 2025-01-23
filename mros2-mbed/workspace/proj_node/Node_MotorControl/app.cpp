7c11661cfdd0b2fa23c761b64e9a1f5a02fe242e
View A Jirapat <viewjirapat@gmail.com>
2025-01-23 16:43:57 +0700

    recomment

--- mros2-mbed/workspace/proj_node/Node_MotorControl/app.cpp
+++ mros2-mbed/workspace/proj_node/Node_MotorControl/app.cpp
@@ -1,70 +1,70 @@
-// /* mros2 example
-//  * Copyright (c) 2022 mROS-base
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  *     http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  */
+/* mros2 example
+ * Copyright (c) 2022 mROS-base
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-// #include "mbed.h"
-// #include "mros2.h"
-// #include "mros2-platform.h"
-// #include "std_msgs/msg/string.hpp"
-// #include <std_msgs/msg/float32.hpp>
+#include "mbed.h"
+#include "mros2.h"
+#include "mros2-platform.h"
+#include "std_msgs/msg/string.hpp"
+#include <std_msgs/msg/float32.hpp>
 
-// void rocon_FdirectCallback(std_msgs::msg::UInt16 *msg);
-// void rocon_angleCallback(std_msgs::msg::Float *msg);
-// void rocon_speedCallback(std_msgs::msg::UInt16 *msg);
-// void rocon_BdirectCallback(std_msgs::msg::UInt16 *msg);
+void rocon_FdirectCallback(std_msgs::msg::UInt16 *msg);
+void rocon_angleCallback(std_msgs::msg::Float *msg);
+void rocon_speedCallback(std_msgs::msg::UInt16 *msg);
+void rocon_BdirectCallback(std_msgs::msg::UInt16 *msg);
 
-// int main()
-// {
-//   if (mros2_platform::network_connect())
-//   {
-//     MROS2_ERROR("failed to connect and setup network! aborting,,,");
-//     return -1;
-//   }
-//   else
-//   {
-//     MROS2_INFO("successfully connect and setup network\r\n---");
-//   }
+int main()
+{
+  if (mros2_platform::network_connect())
+  {
+    MROS2_ERROR("failed to connect and setup network! aborting,,,");
+    return -1;
+  }
+  else
+  {
+    MROS2_INFO("successfully connect and setup network\r\n---");
+  }
 
-//   MROS2_INFO("%s start!", MROS2_PLATFORM_NAME);
-//   MROS2_INFO("app name: Sub_From_RPI4_V2");
+  MROS2_INFO("%s start!", MROS2_PLATFORM_NAME);
+  MROS2_INFO("app name: Sub_From_RPI4_V2");
 
-//   mros2::init(0, NULL);
-//   MROS2_DEBUG("mROS 2 initialization is completed");
+  mros2::init(0, NULL);
+  MROS2_DEBUG("mROS 2 initialization is completed");
 
-//   mros2::Node node = mros2::Node::create_node("mros2_node");
-//   mros2::Subscriber sub_Fdirect = node.create_subscription<std_msgs::msg::UInt16>("pub_rocon_Fdirec", 10, rocon_FdirectCallback);
-//   mros2::Subscriber sub_angle = node.create_subscription<std_msgs::msg::Float32>("pub_rocon_angle", 10, rocon_angleCallback);
-//   mros2::Subscriber sub_speed = node.create_subscription<std_msgs::msg::UInt16>("pub_rocon_speed", 10, rocon_speedCallback);
-//   mros2::Subscriber sub_Bdirect = node.create_subscription<std_msgs::msg::UInt16>("pub_rocon_Bdirec", 10, rocon_BdirectCallback);
+  mros2::Node node = mros2::Node::create_node("mros2_node");
+  mros2::Subscriber sub_Fdirect = node.create_subscription<std_msgs::msg::UInt16>("pub_rocon_Fdirec", 10, rocon_FdirectCallback);
+  mros2::Subscriber sub_angle = node.create_subscription<std_msgs::msg::Float32>("pub_rocon_angle", 10, rocon_angleCallback);
+  mros2::Subscriber sub_speed = node.create_subscription<std_msgs::msg::UInt16>("pub_rocon_speed", 10, rocon_speedCallback);
+  mros2::Subscriber sub_Bdirect = node.create_subscription<std_msgs::msg::UInt16>("pub_rocon_Bdirec", 10, rocon_BdirectCallback);
 
-//   osDelay(1000);
-//   MROS2_INFO("ready to pub/sub message\r\n---");
+  osDelay(1000);
+  MROS2_INFO("ready to pub/sub message\r\n---");
 
-//   mros2::spin();
-//   return 0;
-// }
+  mros2::spin();
+  return 0;
+}
 
-// void rocon_FdirectCallback(std_msgs::msg::UInt16 *msg) {
-//     MROS2_INFO("subscribed Front Direct msg: '%s'\r\n", msg->data.c_str());
-// }
-// void rocon_angleCallback(std_msgs::msg::Float32 *msg) {
-//     MROS2_INFO("subscribed Angle msg: '%s'\r\n", msg->data.c_str());
-// }
-// void rocon_speedCallback(std_msgs::msg::UInt16 *msg) {
-//     MROS2_INFO("subscribed Speed msg: '%s'\r\n", msg->data.c_str());
-// }
-// void rocon_BdirectCallback(std_msgs::msg::UInt16 *msg) {
-//     MROS2_INFO("subscribed Back Direct msg: '%s'\r\n", msg->data.c_str());
-// }
+void rocon_FdirectCallback(std_msgs::msg::UInt16 *msg) {
+    MROS2_INFO("subscribed Front Direct msg: '%s'\r\n", msg->data.c_str());
+}
+void rocon_angleCallback(std_msgs::msg::Float32 *msg) {
+    MROS2_INFO("subscribed Angle msg: '%s'\r\n", msg->data.c_str());
+}
+void rocon_speedCallback(std_msgs::msg::UInt16 *msg) {
+    MROS2_INFO("subscribed Speed msg: '%s'\r\n", msg->data.c_str());
+}
+void rocon_BdirectCallback(std_msgs::msg::UInt16 *msg) {
+    MROS2_INFO("subscribed Back Direct msg: '%s'\r\n", msg->data.c_str());
+}

4a341df5020665dab36829b08d38782d435051af
View A Jirapat <viewjirapat@gmail.com>
2025-02-06 20:54:48 +0700

    ROS 2 Base : Update Node command -> using service instead pub/sub

--- ros2-base-ws/src/package_base_command/src/node_command.cpp
+++ ros2-base-ws/src/package_base_command/src/node_command.cpp
@@ -2,8 +2,8 @@
 #include <std_msgs/msg/string.hpp>
 #include <std_msgs/msg/u_int8.hpp>
 #include <std_msgs/msg/float32_multi_array.hpp>
-#include <ifaces_position/srv/des_data.hpp>
-#include <ifaces_position/msg/spd_limit.hpp>
+#include <ifaces_base/srv/des_data.hpp>
+#include <ifaces_base/srv/spd_limit.hpp>
 #include <cstdlib>
 #include <ctime>
 
@@ -11,41 +11,52 @@ class Node_Command : public rclcpp::Node {
 public:
     Node_Command()
     : Node("node_command") {
-        
+        spd_client_ = this->create_client<ifaces_base::srv::SpdLimit>("spd_limit");
+        des_client_ = this->create_client<ifaces_base::srv::DesData>("des_data");
 
         timer_ = this->create_wall_timer(
             std::chrono::seconds(1), 
             std::bind(&Node_Command::publish_parameters, this)
         );
 
-        RCLCPP_INFO(this->get_logger(), "Version : A");
+        RCLCPP_INFO(this->get_logger(), "Command Node is running...");
     }
 
 private:
-    uint8_t generate_random_speedlimit() {
-        return static_cast<uint8_t>(10 + std::rand() % 50); // Generate random number between 10 and 50
-    }
-
-    void publish_parameters() {
-        speedlimit_ = generate_random_speedlimit();
-        auto spd_lmt_msg = std_msgs::msg::UInt8();
-        spd_lmt_msg.data = speedlimit_;
-        RCLCPP_INFO(this->get_logger(), "Publishing to topic_speedlimit: '%s'", std::to_string(spd_lmt_msg.data).c_str());
-        topic_spd_pub_->publish(spd_lmt_msg);
-
-        destination_ = {1.0f, 2.0f}; // Example data
-        auto des_msg = std_msgs::msg::Float32MultiArray();
-        des_msg.data = destination_; 
-        RCLCPP_INFO(this->get_logger(), "Publishing to topic_destination: '[1.0, 2.0]'");
-        topic_des_pub->publish(des_msg);
-    }
-
-    uint8_t speedlimit_;
-    std::vector<float> destination_;
-
-    rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr topic_spd_pub_;
-    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr topic_des_pub;
+    rclcpp::Client<ifaces_base::srv::SpdLimit>::SharedPtr spd_client_;
+    rclcpp::Client<ifaces_base::srv::DesData>::SharedPtr des_client_;
     rclcpp::TimerBase::SharedPtr timer_;
+
+    void send_service_requests(){
+        auto speed_request = std::make_shared<your_package::srv::SetSpeedLimit::Request>();
+        speed_request->rover_spd = 30;
+
+        auto destination_request = std::make_shared<your_package::srv::SetDestination::Request>();
+        destination_request->des_lat = 12.345;
+        destination_request->des_long = 67.890;
+
+        if (speed_client_->wait_for_service(std::chrono::seconds(2))) {
+            auto future = speed_client_->async_send_request(speed_request);
+            future.then([this](decltype(future) result) {
+                if (result.valid()) {
+                    RCLCPP_INFO(this->get_logger(), "Speed Service Response: %s", result.get()->spd_result.c_str());
+                }
+            });
+        } else {
+            RCLCPP_WARN(this->get_logger(), "Speed Service unavailable.");
+        }
+    
+        if (destination_client_->wait_for_service(std::chrono::seconds(2))) {
+            auto future = destination_client_->async_send_request(destination_request);
+            future.then([this](decltype(future) result) {
+                if (result.valid()) {
+                    RCLCPP_INFO(this->get_logger(), "Destination Service Response: %s", result.get()->result_fser.c_str());
+                }
+            });
+        } else {
+            RCLCPP_WARN(this->get_logger(), "Destination Service unavailable.");
+        }
+    }
 };
 
 int main(int argc, char *argv[]) {

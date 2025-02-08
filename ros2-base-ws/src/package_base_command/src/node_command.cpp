e9c34be864ae18132ad5e10128d21dfe31905459
View A Jirapat <viewjirapat@gmail.com>
2025-02-08 16:49:02 +0700

    ROS 2 Base : UPdate service directory

--- ros2-base-ws/src/package_base_command/src/node_command.cpp
+++ ros2-base-ws/src/package_base_command/src/node_command.cpp
@@ -2,8 +2,8 @@
 #include <std_msgs/msg/string.hpp>
 #include <std_msgs/msg/u_int8.hpp>
 #include <std_msgs/msg/float32_multi_array.hpp>
-#include <ifaces_base/srv/des_data.hpp>
-#include <ifaces_base/srv/spd_limit.hpp>
+#include <service_ifaces/srv/des_data.hpp>
+#include <service_ifaces/srv/spd_limit.hpp>
 #include <cstdlib>
 #include <ctime>
 
@@ -11,25 +11,25 @@ class Node_Command : public rclcpp::Node {
 public:
     Node_Command(float rover_spd, float des_lat, float des_long)
     : Node("node_command"), rover_spd_(rover_spd), des_lat_(des_lat), des_long_(des_long) {
-        spd_client_ = this->create_client<ifaces_base::srv::SpdLimit>("spd_limit");
-        des_client_ = this->create_client<ifaces_base::srv::DesData>("des_data");
+        spd_client_ = this->create_client<service_ifaces::srv::SpdLimit>("spd_limit");
+        des_client_ = this->create_client<service_ifaces::srv::DesData>("des_data");
 
         RCLCPP_INFO(this->get_logger(), "Command Node is running...");
         send_service_requests();
     }
 
 private:
-    rclcpp::Client<ifaces_base::srv::SpdLimit>::SharedPtr spd_client_;
-    rclcpp::Client<ifaces_base::srv::DesData>::SharedPtr des_client_;
+    rclcpp::Client<service_ifaces::srv::SpdLimit>::SharedPtr spd_client_;
+    rclcpp::Client<service_ifaces::srv::DesData>::SharedPtr des_client_;
     float rover_spd_;
     float des_lat_;
     float des_long_;
 
     void send_service_requests() {
-        auto speed_request = std::make_shared<ifaces_base::srv::SpdLimit::Request>();
+        auto speed_request = std::make_shared<service_ifaces::srv::SpdLimit::Request>();
         speed_request->rover_spd = rover_spd_;
 
-        auto destination_request = std::make_shared<ifaces_base::srv::DesData::Request>();
+        auto destination_request = std::make_shared<service_ifaces::srv::DesData::Request>();
         destination_request->des_lat = des_lat_;
         destination_request->des_long = des_long_;
 
@@ -81,22 +81,22 @@ int main(int argc, char *argv[]) {
 // public:
 //     Node_Command()
 //     : Node("node_command") {
-//         spd_client_ = this->create_client<ifaces_base::srv::SpdLimit>("spd_limit");
-//         des_client_ = this->create_client<ifaces_base::srv::DesData>("des_data");
+//         spd_client_ = this->create_client<service_ifaces::srv::SpdLimit>("spd_limit");
+//         des_client_ = this->create_client<service_ifaces::srv::DesData>("des_data");
 
 //         RCLCPP_INFO(this->get_logger(), "Command Node is running...");
 //     }
 
 // private:
-//     rclcpp::Client<ifaces_base::srv::SpdLimit>::SharedPtr spd_client_;
-//     rclcpp::Client<ifaces_base::srv::DesData>::SharedPtr des_client_;
+//     rclcpp::Client<service_ifaces::srv::SpdLimit>::SharedPtr spd_client_;
+//     rclcpp::Client<service_ifaces::srv::DesData>::SharedPtr des_client_;
 //     rclcpp::TimerBase::SharedPtr timer_;
 
 //     void send_service_requests(){
-//         auto speed_request = std::make_shared<ifaces_base::srv::SpdLimit::Request>();
+//         auto speed_request = std::make_shared<service_ifaces::srv::SpdLimit::Request>();
 //         speed_request->rover_spd = 30;
 
-//         auto destination_request = std::make_shared<ifaces_base::srv::DesData::Request>();
+//         auto destination_request = std::make_shared<service_ifaces::srv::DesData::Request>();
 //         destination_request->des_lat = 0.00;
 //         destination_request->des_long = 0.00;
 

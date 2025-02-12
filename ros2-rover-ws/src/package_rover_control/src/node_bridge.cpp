31663d6385a8912d0ac9dec92d5ee2051ffc38e5
View A Jirapat <viewjirapat@gmail.com>
2025-02-12 19:56:24 +0700

    string

--- ros2-rover-ws/src/package_rover_control/src/node_bridge.cpp
+++ ros2-rover-ws/src/package_rover_control/src/node_bridge.cpp
@@ -1,83 +1,153 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include <msgs_mainrocon/msg/main_rocon.hpp>
-#include <msgs_rovercon/msg/sub_rocon.hpp>
 
-class NodeBridge : public rclcpp::Node {
-    public:
-        NodeBridge() : Node("node_bridge") {
-            rclcpp::Context::SharedPtr context_sub = std::make_shared<rclcpp::Context>();
-            rclcpp::InitOptions init_options_sub;
-            init_options_sub.set_domain_id(2);  // Domain ID 2 for Subscriber
-            context_sub->init(0, nullptr, init_options_sub);
+class CombinedNode : public rclcpp::Node
+{
+public:
+    CombinedNode() : Node("combined_node")
+    {
+        // Set Domain ID for Subscriber
+        rclcpp::Context::SharedPtr context_sub = std::make_shared<rclcpp::Context>();
+        rclcpp::InitOptions init_options_sub;
+        init_options_sub.set_domain_id(1);  // Domain ID 1 for Subscriber
+        context_sub->init(0, nullptr, init_options_sub);
 
-            sub_node_ = std::make_shared<rclcpp::Node>("sub_node", init_options_sub);
-            subscription_ = sub_node_->create_subscription<msgs_mainrocon::msg::MainRocon>(
-                "pub_rovercontrol", 10, std::bind(&NodeBridge::topic_callback, this, std::placeholders::_1));
+        sub_node_ = std::make_shared<rclcpp::Node>("sub_node", init_options_sub);
+        subscription_ = sub_node_->create_subscription<std_msgs::msg::String>(
+            "/topic_a", 10, std::bind(&CombinedNode::topic_callback, this, std::placeholders::_1));
 
-            // Set Domain ID for Publisher
-            rclcpp::Context::SharedPtr context_pub = std::make_shared<rclcpp::Context>();
-            rclcpp::InitOptions init_options_pub;
-            init_options_pub.set_domain_id(1);  // Domain ID 1 for Publisher
-            context_pub->init(0, nullptr, init_options_pub);
+        // Set Domain ID for Publisher
+        rclcpp::Context::SharedPtr context_pub = std::make_shared<rclcpp::Context>();
+        rclcpp::InitOptions init_options_pub;
+        init_options_pub.set_domain_id(2);  // Domain ID 2 for Publisher
+        context_pub->init(0, nullptr, init_options_pub);
 
-            pub_node_ = std::make_shared<rclcpp::Node>("pub_node", init_options_pub);
-            publisher_ = pub_node_->create_publisher<msgs_mainrocon::msg::MainRocon>("pub_rovercontrol_d1", 10);
+        pub_node_ = std::make_shared<rclcpp::Node>("pub_node", init_options_pub);
+        publisher_ = pub_node_->create_publisher<std_msgs::msg::String>("/topic_b", 10);
 
-            RCLCPP_INFO(this->get_logger(), "Combined node initialized (Sub: Domain 1, Pub: Domain 2)");
+        RCLCPP_INFO(this->get_logger(), "Combined node initialized (Sub: Domain 1, Pub: Domain 2)");
 
-            executor_.add_node(sub_node_);
-            executor_.add_node(pub_node_);
-            executor_thread_ = std::thread([this]() { executor_.spin(); });
-        }
-
-        ~NodeBridge()
-        {
-            executor_.cancel();
-            if (executor_thread_.joinable())
-                executor_thread_.join();
-        }
-        
-    private:
-        void topic_callback(const msgs_mainrocon::msg::MainRocon::SharedPtr msg)
-        {
-            RCLCPP_INFO(this->get_logger(), "Received on /pub_rovercontrol");
-
-            // Publish received message to /topic_b
-            auto new_mainmsg = msgs_mainrocon::msg::MainRocon();
-            auto new_msg = msgs_rovercon::msg::SubRocon();
-        
-            new_msg.fdr_msg = msg->mainrocon_msg.fdr_msg;
-            new_msg.ro_ctrl_msg = msg->mainrocon_msg.ro_ctrl_msg;
-            new_msg.spd_msg = msg->mainrocon_msg.spd_msg;
-            new_msg.bdr_msg = msg->mainrocon_msg.bdr_msg;
-
-            new_mainmsg.mainrocon_msg = new_msg;
-
-            publisher_->publish(new_mainmsg);
-
-            RCLCPP_INFO(this->get_logger(), "Forwarded message to /pub_rovercontrol_d1: [%d, %.2f, %d, %d]", 
-                new_mainmsg.mainrocon_msg.fdr_msg, 
-                new_mainmsg.mainrocon_msg.ro_ctrl_msg, 
-                new_mainmsg.mainrocon_msg.spd_msg, 
-                new_mainmsg.mainrocon_msg.bdr_msg);
-        }
-
-        rclcpp::Node::SharedPtr sub_node_;
-        rclcpp::Node::SharedPtr pub_node_;
-        rclcpp::Subscription<msgs_mainrocon::msg::MainRocon>::SharedPtr subscription_;
-        rclcpp::Publisher<msgs_mainrocon::msg::MainRocon>::SharedPtr publisher_;
-        rclcpp::executors::MultiThreadedExecutor executor_;
-        std::thread executor_thread_;
-
-};
-    int main(int argc, char *argv[]) {
-        rclcpp::init(argc, argv);
-        auto node = std::make_shared<NodeBridge>();
-        rclcpp::spin(node);
-        rclcpp::shutdown();
-        return 0;
+        executor_.add_node(sub_node_);
+        executor_.add_node(pub_node_);
+        executor_thread_ = std::thread([this]() { executor_.spin(); });
     }
+
+    ~CombinedNode()
+    {
+        executor_.cancel();
+        if (executor_thread_.joinable())
+            executor_thread_.join();
+    }
+
+private:
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+    {
+        RCLCPP_INFO(this->get_logger(), "Received on /topic_a: '%s'", msg->data.c_str());
+
+        // Publish received message to /topic_b
+        auto new_msg = std_msgs::msg::String();
+        new_msg.data = "Relayed: " + msg->data;
+        publisher_->publish(new_msg);
+        RCLCPP_INFO(this->get_logger(), "Published on /topic_b: '%s'", new_msg.data.c_str());
+    }
+
+    rclcpp::Node::SharedPtr sub_node_;
+    rclcpp::Node::SharedPtr pub_node_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::executors::MultiThreadedExecutor executor_;
+    std::thread executor_thread_;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<CombinedNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
+
+// #include "rclcpp/rclcpp.hpp"
+// #include "std_msgs/msg/string.hpp"
+// #include <msgs_mainrocon/msg/main_rocon.hpp>
+// #include <msgs_rovercon/msg/sub_rocon.hpp>
+
+// class NodeBridge : public rclcpp::Node {
+//     public:
+//         NodeBridge() : Node("node_bridge") {
+//             rclcpp::Context::SharedPtr context_sub = std::make_shared<rclcpp::Context>();
+//             rclcpp::InitOptions init_options_sub;
+//             init_options_sub.set_domain_id(2);  // Domain ID 2 for Subscriber
+//             context_sub->init(0, nullptr, init_options_sub);
+
+//             sub_node_ = std::make_shared<rclcpp::Node>("sub_node", init_options_sub);
+//             subscription_ = sub_node_->create_subscription<msgs_mainrocon::msg::MainRocon>(
+//                 "pub_rovercontrol", 10, std::bind(&NodeBridge::topic_callback, this, std::placeholders::_1));
+
+//             // Set Domain ID for Publisher
+//             rclcpp::Context::SharedPtr context_pub = std::make_shared<rclcpp::Context>();
+//             rclcpp::InitOptions init_options_pub;
+//             init_options_pub.set_domain_id(1);  // Domain ID 1 for Publisher
+//             context_pub->init(0, nullptr, init_options_pub);
+
+//             pub_node_ = std::make_shared<rclcpp::Node>("pub_node", init_options_pub);
+//             publisher_ = pub_node_->create_publisher<msgs_mainrocon::msg::MainRocon>("pub_rovercontrol_d1", 10);
+
+//             RCLCPP_INFO(this->get_logger(), "Combined node initialized (Sub: Domain 1, Pub: Domain 2)");
+
+//             executor_.add_node(sub_node_);
+//             executor_.add_node(pub_node_);
+//             executor_thread_ = std::thread([this]() { executor_.spin(); });
+//         }
+
+//         ~NodeBridge()
+//         {
+//             executor_.cancel();
+//             if (executor_thread_.joinable())
+//                 executor_thread_.join();
+//         }
+        
+//     private:
+//         void topic_callback(const msgs_mainrocon::msg::MainRocon::SharedPtr msg)
+//         {
+//             RCLCPP_INFO(this->get_logger(), "Received on /pub_rovercontrol");
+
+//             // Publish received message to /topic_b
+//             auto new_mainmsg = msgs_mainrocon::msg::MainRocon();
+//             auto new_msg = msgs_rovercon::msg::SubRocon();
+        
+//             new_msg.fdr_msg = msg->mainrocon_msg.fdr_msg;
+//             new_msg.ro_ctrl_msg = msg->mainrocon_msg.ro_ctrl_msg;
+//             new_msg.spd_msg = msg->mainrocon_msg.spd_msg;
+//             new_msg.bdr_msg = msg->mainrocon_msg.bdr_msg;
+
+//             new_mainmsg.mainrocon_msg = new_msg;
+
+//             publisher_->publish(new_mainmsg);
+
+//             RCLCPP_INFO(this->get_logger(), "Forwarded message to /pub_rovercontrol_d1: [%d, %.2f, %d, %d]", 
+//                 new_mainmsg.mainrocon_msg.fdr_msg, 
+//                 new_mainmsg.mainrocon_msg.ro_ctrl_msg, 
+//                 new_mainmsg.mainrocon_msg.spd_msg, 
+//                 new_mainmsg.mainrocon_msg.bdr_msg);
+//         }
+
+//         rclcpp::Node::SharedPtr sub_node_;
+//         rclcpp::Node::SharedPtr pub_node_;
+//         rclcpp::Subscription<msgs_mainrocon::msg::MainRocon>::SharedPtr subscription_;
+//         rclcpp::Publisher<msgs_mainrocon::msg::MainRocon>::SharedPtr publisher_;
+//         rclcpp::executors::MultiThreadedExecutor executor_;
+//         std::thread executor_thread_;
+
+// };
+//     int main(int argc, char *argv[]) {
+//         rclcpp::init(argc, argv);
+//         auto node = std::make_shared<NodeBridge>();
+//         rclcpp::spin(node);
+//         rclcpp::shutdown();
+//         return 0;
+//     }
 // #include <rclcpp/rclcpp.hpp>
 // #include <msgs_mainrocon/msg/main_rocon.hpp>
 // #include <queue>

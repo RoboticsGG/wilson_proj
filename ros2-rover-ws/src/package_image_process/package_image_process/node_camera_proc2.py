81bde5ac1fcf77a55bc944ef5072783d728446c6
View A Jirapat <viewjirapat@gmail.com>
2025-02-13 23:41:32 +0700

    reduce fps to 15

--- ros2-rover-ws/src/package_image_process/package_image_process/node_camera_proc2.py
+++ ros2-rover-ws/src/package_image_process/package_image_process/node_camera_proc2.py
@@ -92,8 +92,8 @@ class ImageProcess(Node):
     def read_camera_continuously(self):
         pipeline = rs.pipeline()
         config = rs.config()
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 15)
 
         ctx = rs.context()
         devices = ctx.query_devices()

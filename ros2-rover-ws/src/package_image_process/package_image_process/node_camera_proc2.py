9ea4c3fde31ce604038a299a0a69e8539c3c312b
View A Jirapat <viewjirapat@gmail.com>
2025-02-13 23:39:00 +0700

    Update Resolution

--- ros2-rover-ws/src/package_image_process/package_image_process/node_camera_proc2.py
+++ ros2-rover-ws/src/package_image_process/package_image_process/node_camera_proc2.py
@@ -92,8 +92,8 @@ class ImageProcess(Node):
     def read_camera_continuously(self):
         pipeline = rs.pipeline()
         config = rs.config()
-        config.enable_stream(rs.stream.color, 640, 360, rs.format.bgr8, 30)
-        config.enable_stream(rs.stream.depth, 640, 360, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
         ctx = rs.context()
         devices = ctx.query_devices()

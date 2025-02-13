a9859aa4089d3c27e221a0d6a2c54c836eb55f13
View A Jirapat <viewjirapat@gmail.com>
2025-02-13 13:38:12 +0700

    test pub d

--- ros2-base-ws/src/package_test/src/node_pub_d.cpp
+++ ros2-base-ws/src/package_test/src/node_pub_d.cpp
@@ -3,41 +3,47 @@
 #include <chrono>
 #include <memory>
 #include <string>
+#include <msgs_rovercon/msg/sub_rocon.hpp>
+#include <msgs_mainrocon/msg/main_rocon.hpp>
 
-using namespace std::chrono_literals;
 
 class PubNode : public rclcpp::Node {
 public:
-    PubNode(const std::string& name, int domain_id)
-        : Node(name) {
-        auto publisher = this->create_publisher<std_msgs::msg::String>("topic_dn", 10);
+    PubNode() : Node('test_pub') {
+      topic_rocon_pub_ = this->create_publisher<msgs_mainrocon::msg::MainRocon>("pub_rovercontrol", 10);
 
-        auto timer_callback = [publisher, name, domain_id]() {
-            auto message = std_msgs::msg::String();
-            message.data = "Hello from " + name + " (Domain " + std::to_string(domain_id) + ")";
-            RCLCPP_INFO(rclcpp::get_logger(name), "Publishing: '%s'", message.data.c_str());
-            publisher->publish(message);
-        };
-
-        timer_ = this->create_wall_timer(500ms, timer_callback);
+      timer_ = this->create_wall_timer(
+        std::chrono::seconds(2), 
+        std::bind(&PubNode::timer_callback, this)
+    );
+      RCLCPP_INFO(this->get_logger(), "Version : A");
     }
 
 private:
+    rclcpp::Publisher<msgs_mainrocon::msg::MainRocon>::SharedPtr topic_rocon_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
+
+    void timer_callback() {
+      auto subrocon = msgs_rovercon::msg::SubRocon();
+      auto mainrocon = msgs_mainrocon::msg::MainRocon();
+      
+      {
+          std::lock_guard<std::mutex> lock(data_lock_);
+
+          subrocon.fdr_msg = 2;
+          subrocon.ro_ctrl_msg = 12.20;
+          subrocon.spd_msg = 20;
+          subrocon.bdr_msg = 2; // 1 = fw, 2 = bw, 0 = stop.
+          
+          mainrocon.mainrocon_msg = subrocon;
+      }
+      topic_rocon_pub_->publish(mainrocon);
+    }
 };
 
 int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);  // Initialize ROS 2 only once
-
-    auto node1 = std::make_shared<PubNode>("node1", 1);
-    auto node2 = std::make_shared<PubNode>("node2", 2);
-
-    rclcpp::executors::MultiThreadedExecutor executor;
-    executor.add_node(node1);
-    executor.add_node(node2);
-
-    executor.spin();  // Run both nodes in the same process
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<PubNode>());
     rclcpp::shutdown();
-
     return 0;
 }

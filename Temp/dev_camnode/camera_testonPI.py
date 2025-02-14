ce416a857e0cbec86bec5932bb49cee99ecba672
View A Jirapat <viewjirapat@gmail.com>
2025-02-14 15:25:51 +0700

    test 5 fps

--- Temp/dev_camnode/camera_testonPI.py
+++ Temp/dev_camnode/camera_testonPI.py
@@ -7,7 +7,7 @@ pipeline = rs.pipeline()
 config = rs.config()
 
 # Set the stream parameters
-config.enable_stream(rs.stream.color, 424, 240, rs.format.bgr8, 15)
+config.enable_stream(rs.stream.color, 424, 240, rs.format.bgr8, 5)
 #config.enable_stream(rs.stream.depth, 424, 240, rs.format.z16, 15)
 
 # Start the pipeline
@@ -15,7 +15,8 @@ pipeline.start(config)
 
 try:
     while True:
-        frames = pipeline.wait_for_frames(timeout_ms=10000)
+        frames = pipeline.wait_for_frames()
+        #frames = pipeline.wait_for_frames(timeout_ms=10000)
         color_frame = frames.get_color_frame()
         #depth_frame = frames.get_depth_frame()
 

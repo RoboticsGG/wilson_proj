62685f13b4748c5e69a33d98afecf646e1851f40
View A Jirapat <viewjirapat@gmail.com>
2025-02-14 13:34:18 +0700

    add time out and reduce frame test

--- Temp/dev_camnode/camera_testonPI.py
+++ Temp/dev_camnode/camera_testonPI.py
@@ -7,30 +7,30 @@ pipeline = rs.pipeline()
 config = rs.config()
 
 # Set the stream parameters
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-#config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 15)
 
 # Start the pipeline
 pipeline.start(config)
 
 try:
     while True:
-        frames = pipeline.wait_for_frames()
+        frames = pipeline.wait_for_frames(timeout_ms=10000)
         color_frame = frames.get_color_frame()
-        #depth_frame = frames.get_depth_frame()
+        depth_frame = frames.get_depth_frame()
 
-        # if not color_frame or not depth_frame:
-        #     continue
-
-        if not color_frame :
+        if not color_frame or not depth_frame:
             continue
 
+        # if not color_frame :
+        #     continue
+
         color_image = np.asanyarray(color_frame.get_data())
-        #depth_image = np.asanyarray(depth_frame.get_data())
+        depth_image = np.asanyarray(depth_frame.get_data())
 
         # Display images
         cv2.imshow('Color Frame', color_image)
-        #cv2.imshow('Depth Frame', depth_image)
+        cv2.imshow('Depth Frame', depth_image)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break

eacf428279dbc7ea0f5c801e73434dd2b1811c55
View A Jirapat <viewjirapat@gmail.com>
2025-02-17 00:31:17 +0700

    TT

--- ros2_base_setup.sh
+++ ros2_base_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #ROS2_BASE_SETUP#
-cd wilson_proj/ros2-base-ws || exit 1
+cd ros2-base-ws/ || exit 1
 sudo rm -rf build/ install/ log/ || exit 1
 colcon build --packages-select action_ifaces service_ifaces msgs_rovercon || exit 1
 source install/setup.bash || exit 1

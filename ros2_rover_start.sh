34f3b08de703e5bd276c5837b620f06189555348
View A Jirapat <viewjirapat@gmail.com>
2025-02-17 00:42:17 +0700

    update scripts

--- ros2_rover_start.sh
+++ ros2_rover_start.sh
@@ -4,15 +4,15 @@
 source ./ros2-rover-ws/install/setup.bash || exit 1
 export ROS_DOMAIN_ID=2
 
-# Run the nodes in the background
-ros2 run package_position_process node_gnss & || exit 1
-ros2 run package_position_process node_position_process & || exit 1
-ros2 run package_image_process node_camera_proc2 & || exit 1
-ros2 run package_rover_control node_rovercontrol & || exit 1
+# Run all nodes in the background simultaneously
+ros2 run package_position_process node_gnss & 
+ros2 run package_position_process node_position_process & 
+ros2 run package_image_process node_camera_proc2 & 
+ros2 run package_rover_control node_rovercontrol &
 
 # Switch ROS_DOMAIN_ID and run the last node in the background
 export ROS_DOMAIN_ID=5
-ros2 run package_rover_control node_d5bridge & || exit 1
+ros2 run package_rover_control node_d5bridge &
 
 # Wait for all background jobs to finish before exiting the script
 wait

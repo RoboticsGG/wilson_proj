fabc641b108e40327c8c5bacb9024b8fa10f7073
View A Jirapat <viewjirapat@gmail.com>
2025-02-17 00:51:58 +0700

    update setup.bash

--- ros2_rover_setup.sh
+++ ros2_rover_setup.sh
@@ -3,7 +3,7 @@
 #ROS2_ROVER_SETUP#
 cd ./ros2-rover-ws/ || exit 1
 sudo rm -rf build/ install/ log/ || exit 1
-colcon build --packages-select action_ifaces ifaces_position ifaces_rover msgs_ifaces msgs_rovercon service_ifaces || exit 1
+colcon build --packages-select action_ifaces msgs_ifaces msgs_rovercon service_ifaces || exit 1
 source install/setup.bash || exit 1
 colcon build || exit 1
 source install/setup.bash || exit 1

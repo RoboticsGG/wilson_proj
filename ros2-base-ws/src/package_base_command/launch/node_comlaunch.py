a0c0a7f2b37558c6d3474ddd6c085b2c89b0bb73
View A Jirapat <viewjirapat@gmail.com>
2025-02-16 19:42:55 +0700

    update file launch

--- ros2-base-ws/src/package_base_command/launch/node_comlaunch.py
+++ ros2-base-ws/src/package_base_command/launch/node_comlaunch.py
@@ -1,6 +1,6 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, Shutdown
+from launch.actions import DeclareLaunchArgument, Log, Shutdown
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
@@ -27,9 +27,43 @@ def generate_launch_description():
             on_exit=Shutdown(),         # Ensure proper shutdown when stopping
             emulate_tty=True,           # Fix issues with logs not appearing properly
             output='screen',            # Print logs to terminal
-            shutdown_timeout=20.0   
+            shutdown_timeout=20.0       # Give extra time to properly stop
         )
     ])
+
+#################################################################
+#  import os
+# from launch import LaunchDescription
+# from launch.actions import DeclareLaunchArgument, Shutdown
+# from launch_ros.actions import Node
+# from launch.substitutions import LaunchConfiguration
+
+# def generate_launch_description():
+#     # Declare launch arguments with string values (ROS 2 arguments are passed as strings)
+#     rover_spd_arg = DeclareLaunchArgument('rover_spd', default_value='0', description='Speed of the rover')
+#     des_lat_arg = DeclareLaunchArgument('des_lat', default_value='0.000000', description='Destination Latitude')
+#     des_long_arg = DeclareLaunchArgument('des_long', default_value='0.00000', description='Destination Longitude')
+
+#     return LaunchDescription([
+#         rover_spd_arg,
+#         des_lat_arg,
+#         des_long_arg,
+
+#         Node(
+#             package='package_base_command',
+#             executable='node_command',
+#             name='node_command',
+#             parameters=[{
+#                 'rover_spd': LaunchConfiguration('rover_spd'),
+#                 'des_lat': LaunchConfiguration('des_lat'),
+#                 'des_long': LaunchConfiguration('des_long')
+#             }],
+#             on_exit=Shutdown(),         # Ensure proper shutdown when stopping
+#             emulate_tty=True,           # Fix issues with logs not appearing properly
+#             output='screen',            # Print logs to terminal
+#             shutdown_timeout=20.0   
+#         )
+#     ])
 
 
 ################### Read config from yaml file ###################
